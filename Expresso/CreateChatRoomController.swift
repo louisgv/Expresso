c0916db3e765112159fdef57be4af48ef3ca4529
Alex Mitchell <spartanawesome@gmail.com>
2016-10-22 00:13:05 -0400

    Updated CreateChatController

--- Expresso/CreateChatRoomController.swift
+++ Expresso/CreateChatRoomController.swift
@@ -11,14 +11,58 @@ import UIKit
 class CreateChatRoomController: UIViewController{
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        
         setupNavBar()
+        setupViews()
     }
+    
+    let stressContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .blue
+        return view
+    }()
+   
+    let inputsContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .red
+        return view
+    }()
+    
+    let groupContainer: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .yellow
+        return view
+    }()
     
     func setupNavBar(){
         navigationController?.navigationBar.isTranslucent = false
-        navigationItem.title = "Request help?"
+        navigationItem.title = "Request help"
         navigationController!.navigationBar.titleTextAttributes = [ NSFontAttributeName: UIFont(name: "Avenir", size: 18)!, NSForegroundColorAttributeName : UIColor.white]
         navigationController?.navigationBar.tintColor = .white
+    }
+    
+    func setupViews() {
+        view.addSubview(stressContainer)
+        view.addSubview(inputsContainer)
+        view.addSubview(groupContainer)
+        
+        stressContainer.topAnchor.constraint(equalTo: topLayoutGuide.bottomAnchor).isActive = true
+        stressContainer.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        stressContainer.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        stressContainer.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/3).isActive = true
+        
+        inputsContainer.topAnchor.constraint(equalTo: stressContainer.bottomAnchor).isActive = true
+        inputsContainer.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        inputsContainer.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        inputsContainer.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/3).isActive = true
+        
+        groupContainer.topAnchor.constraint(equalTo: inputsContainer.bottomAnchor).isActive = true
+        groupContainer.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        groupContainer.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        groupContainer.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 1/3).isActive = true
+        
     }
 }
